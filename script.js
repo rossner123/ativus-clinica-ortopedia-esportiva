2e958879fbae5da6bc12bb37a60b8dfe941f443d
Bruno <brumassotti@gmail.com>
2025-06-30 21:33:49 -0300

    termina carrosel convenios

--- script.js
+++ script.js
@@ -37,39 +37,39 @@ function iniciarIntervalo(){
     intervalo = setInterval(() => mudarSlide(1), 3000);
 }
 
+iniciarIntervalo();
+
 let scrollIndex = 0;
-const logosWrapper = document.querySelector(".logosConvenios");
+const logosContainer = document.querySelector(".logosConvenios");
 let logos = document.querySelectorAll(".logo");
 
 function scrollConvenios(direcao) {
-    // Atualiza os logos (caso eles mudem dinamicamente depois)
     logos = document.querySelectorAll(".logo");
 
-    // Corrige limites
     const maxIndex = logos.length - 5;
     if (direcao > 0 && scrollIndex >= maxIndex) return;
     if (direcao < 0 && scrollIndex <= 0) return;
 
-    // Calcula o passo com base no logo atual
     const logoAtual = logos[scrollIndex];
     const larguraLogo = logoAtual.offsetWidth;
-    const gap = 48; // mesmo valor do CSS (gap: 2rem)
+    const gap = 48;
 
-    // Atualiza índice
     scrollIndex += direcao;
 
-    // Move com base no total acumulado até o índice atual
     let deslocamento = 0;
 
     for (let i = 0; i < scrollIndex; i++) {
         deslocamento += logos[i].offsetWidth + gap;
     }
 
-    logosWrapper.style.transform = `translateX(-${deslocamento}px)`;
+    logosContainer.style.transform = `translateX(-${deslocamento}px)`;
 }
 
-console.log(logos)
-console.log(logos[0].offsetWidth)
-console.log(logos.length - 5)
+const links = document.querySelectorAll(".nav-link");
+const caminho = window.location.pathname;
 
-iniciarIntervalo();
+links.forEach(link => {
+  if (link.getAttribute("href") === caminho || caminho.endsWith("/" + link.getAttribute("href"))) {
+    link.classList.add("ativo");
+  }
+});
